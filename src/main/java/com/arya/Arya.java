92198eef9ec9603f3640b2ab9d58f00fd72a5c1c
Kishan <kishanajudiya2910@gmail.com>
2023-04-28 11:52:47 +0200

    added features

--- src/main/java/com/arya/Arya.java
+++ src/main/java/com/arya/Arya.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Arya {
 
-	public void main(String[] args) {
+	void main(String[] args) {
 		
 		SpringApplication.run(Arya.class, args);
 		
